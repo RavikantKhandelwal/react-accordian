82d38179913e41b003e9c1450bf027f4714b43bf
Ravikant Khandelwal <ravikant.khandelwal@bts.com>
2020-12-27 18:04:31 +0530

    Updates

---
+++ precache-manifest.01bae211dde7b7a000a3fb2ff101ba7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a83869e49fbace98ab4628e6f9b7c0",
+    "url": "/react-accordian/index.html"
+  },
+  {
+    "revision": "17679d21ac3808145b86",
+    "url": "/react-accordian/static/css/main.9280bcb1.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/react-accordian/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-accordian/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17679d21ac3808145b86",
+    "url": "/react-accordian/static/js/main.3db41a17.chunk.js"
+  },
+  {
+    "revision": "1b8038ce4c47011bbd85",
+    "url": "/react-accordian/static/js/runtime-main.1ed464fb.js"
+  }
+]);
